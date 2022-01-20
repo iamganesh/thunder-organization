a2aa41dfde45d354f13ca9067ab1fe3270891579
Ganesh Kumar <kganeshkumar996@gmail.com>
2022-01-20 11:33:39 +0530

    added gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
   },[]);
   
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <AuthProvider>
         <Switch>
           <PrivateRoute exact path="/" component={Dashboard} />
